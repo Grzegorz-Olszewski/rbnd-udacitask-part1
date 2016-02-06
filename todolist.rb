86dec1b17be41a257cb951e45f031df3b26ea925
Grzegorz Olszewski <grzegorz.olszewski10@gmail.com>
2016-02-06 20:05:05 +0100

    Things done with prep tutorial.

---
+++ todolist.rb
@@ -0,0 +1,21 @@
+class TodoList
+	attr_reader :title, :items
+	def initialize(list_title)
+		@title = list_title
+		@items = Array.new
+	end
+	def add_item(new_item)
+		new_item = Item.new(new_item)
+		@items.push(new_item)
+	end
+end
+
+
+class Item
+	attr_reader :description, :completed_status
+
+	def initialize(item_description)
+		@description = item_description
+		@completed_status = false
+	end
+end
