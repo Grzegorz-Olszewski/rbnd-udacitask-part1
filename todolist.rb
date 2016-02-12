5186f82872c58743b1b8b88440fa7fb287a75a3e
Grzegorz Olszewski <grzegorz.olszewski10@gmail.com>
2016-02-12 09:12:52 +0100

    Almost done.I have to add some comments, and check everything.

--- todolist.rb
+++ todolist.rb
@@ -9,16 +9,37 @@ class TodoList
 	def change_title(new_title)
 		@title = new_title
 	end
-	def add_item(new_item)
-		item = Item.new(new_item)
-		@items.push(item)
-	end
 	def delete_item(number_in_array)
 		@items.delete_at(number_in_array)
 	end
 	def print_list
+		print "\n"
+		print "------------------------ \n"
+		print "\t#{@title} \n"
+		print "------------------------ \n"
+
 		items.each do |item|
 			item.print_item
+		end
+	end
+	def how_am_i_doing
+		count_all_thing = 0.0
+		count_things_done = 0.0
+		@items.each do |thing| 
+			if thing.completed? == true
+				count_things_done+=1
+				count_all_thing+=1
+			else
+				count_all_thing+=1
+			end
+		end
+		success_ratio = count_things_done/count_all_thing
+		if success_ratio < 0.5
+			puts "Get up, you lazy bastard!"
+		elsif (success_ratio >= 0.5) && (success_ratio < 1.0) 
+			puts "You're doing well! Keep up the good work"
+		else
+			puts "Wow! Are you a robot or something?"
 		end
 	end
 end
@@ -27,9 +48,11 @@ end
 class Item
 	attr_reader :description 
 	attr_accessor :completed_status
-	def initialize(item_description)
+	def initialize(item_description,todolist, due_date = "#{Time.now.strftime("%Y:%m:%d")} ")
 		@description = item_description
 		@completed_status = false
+		@due_date = due_date
+		todolist.items.push(self)
 	end
 	def change_completion_status
 		if @completed_status == false
@@ -46,17 +69,12 @@ class Item
 		end
 	end
 	def print_item 
-		print "Description: #{@description}\n"
-		print "Completed? #{@completed_status}\n"
+		completion_mark = "| |"
+		if @completed_status == true
+			completion_mark = "|✓|"
+		end
+		print "#{completion_mark} #{@due_date} #{@description} \n"
 	end
 
 end
-
-list = TodoList.new ("Greg's list")
-
-list.add_item("do dishes")
-list.add_item("do laundry")
-
-list.print_list
-
-
+ 
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,38 @@
 require_relative "todolist.rb"
-
 # Creates a new todo list
-
+list = TodoList.new("UdaciList")
 # Add four new items
-
+thing1 = Item.new("Become superhuman",list,"Tomorrow")
+thing2 = Item.new("Learn how to fly",list)
+thing3 = Item.new("Do laundry",list)
+thing4 = Item.new("Make a cake",list)
+	
 # Print the list
-
+list.print_list
 # Delete the first item
-
+list.delete_item(0)
 # Print the list
-
+list.print_list
 # Delete the second item
-
+list.delete_item(1)
 # Print the list
-
+list.print_list
 # Update the completion status of the first item to complete
-
+list.items[0].change_completion_status
 # Print the list
-
+list.print_list
 # Update the title of the list
-
+list.change_title("My list")
 # Print the list
+list.print_list
+
+File.open('new_list', 'w+') do |f|  
+  Marshal.dump(list, f)  
+end  
+
+File.open('old_list') do |f|  
+  @old_list = Marshal.load(f)  
+end  
+
+@old_list.print_list
+@old_list.how_am_i_doing
